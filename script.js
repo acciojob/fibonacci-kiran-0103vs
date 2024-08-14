2a7c09abf4b9a0dc22b7a74430e09f706576a673
kiran-0103vs <131228987+kiran-0103vs@users.noreply.github.com>
2024-08-14 21:10:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+	if (num <= 0) {
+        return "Invalid input. num should be a positive integer.";
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        let a = 0, b = 1;
+        for (let i = 3; i <= num; i++) {
+            let temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
 }
 
 module.exports = fibonacci;
